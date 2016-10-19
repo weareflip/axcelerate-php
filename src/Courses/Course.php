94fadf3c9260e59496f1e3b35ba75995777780a7
Joel Shepherd <joel@swivid.ink>
2016-10-19 15:44:50 +1000

    Stub method

--- src/Courses/Course.php
+++ src/Courses/Course.php
@@ -6,5 +6,10 @@ use Flip\Axcelerate\AxcelerateResource;
 
 class Course extends AxcelerateResource
 {
-
+    // @TODO We'll be used is like $course->searchForInstance(['name' => '%$classname%', 'trainerid' => $contact->id])
+    // May be refactored later when we bring in trainers
+    public function searchForInstance($attributes)
+    {
+        //
+    }
 }
